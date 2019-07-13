d0d8747daf5173c78a208abff2904b6d121635b0
vandeurenglenn <vandeurenglenn@gmail.com>
2019-07-13 14:08:44 +0200

    :metal: fix directions url

--- src/shop/notification-listener.js
+++ src/shop/notification-listener.js
@@ -2,7 +2,7 @@ self.addEventListener('notificationclick', ({ action, notification }) => {
   let url;
 
   if (action === 'checkOrder') url = `/#order?=${notification.data}`;
-  else url = 'https://maps.google.com/maps?q=guldentopveldwinkel&t=&z=17&ie=UTF8&iwloc=&output=embed';
+  else url = 'https://www.google.com/maps/dir/50.9804131,4.7489457/@50.980413,4.748946,17z?hl=en-GB';
 
   self.clients.openWindow(url);
 });
